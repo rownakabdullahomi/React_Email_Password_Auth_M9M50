4ba9976cc3a73f21cdaf9a782a3bc6bfc00ecf1d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-10 21:39:58 +0600

    password show and hidden done terms checkbox added

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,44 +1,52 @@
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.init";
 import { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 const SignUp = () => {
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-    const [successMessage, setSuccessMessage] = useState(false)
-    const [errorMessage, setErrorMessage] = useState("")
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const terms = e.target.terms.checked;
 
-    const handleSignUp = (e)=>{
-        e.preventDefault();
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        
-        setErrorMessage("");
-        setSuccessMessage(false)
+    setErrorMessage("");
+    setSuccessMessage(false);
 
-        if(password.length < 6){
-            setErrorMessage("Password must be 6 characters.");
-            return;
-        }
-
-        const passwordRegex = /^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[\W_])).{6,}$/;
-
-        if(!passwordRegex.test(password)){
-            setErrorMessage("At least one number, one uppercase, one lowercase and one special character");
-            return;
-        }
-
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(result => {
-            console.log(result);
-            setSuccessMessage(true);
-        })
-        .catch(error => {
-            setSuccessMessage(false);
-            setErrorMessage(error.message);
-            console.log(errorMessage);
-            
-        })
+    if(!terms){
+        setErrorMessage("Accept Terms and Conditions");
     }
+
+    if (password.length < 6) {
+      setErrorMessage("Password must be 6 characters.");
+      return;
+    }
+
+    const passwordRegex =
+      /^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*\d))(?=(.*[\W_])).{6,}$/;
+
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "At least one number, one uppercase, one lowercase and one special character"
+      );
+      return;
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        console.log(result);
+        setSuccessMessage(true);
+      })
+      .catch((error) => {
+        setSuccessMessage(false);
+        setErrorMessage(error.message);
+        console.log(errorMessage);
+      });
+  };
 
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto my-8">
@@ -56,35 +64,46 @@ const SignUp = () => {
             required
           />
         </div>
-        <div className="form-control">
+        <div className="form-control relative">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
           <input
-            type="password"
+            type={showPassword ? "text" : "password"}
             name="password"
             placeholder="password"
             className="input input-bordered"
             required
           />
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className="btn btn-xs absolute right-2 top-12"
+          >
+            {showPassword ? <FaEyeSlash /> : <FaEye />}
+          </button>
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">
               Forgot password?
             </a>
           </label>
         </div>
+
+        <div className="form-control">
+          <label className="label cursor-pointer justify-start gap-2">
+            <input type="checkbox" name="terms" className="checkbox" />
+            <span className="label-text">Accept Our Terms and Conditions</span>
+          </label>
+        </div>
+
         <div className="form-control mt-6">
           <button className="btn btn-primary">Sign Up</button>
         </div>
       </form>
 
-        {
-            errorMessage && <p className="text-red-600">{errorMessage}</p>
-        }
-        {
-            successMessage && <p className="text-green-600">Sign Up is Successful.</p>
-        }
-
+      {errorMessage && <p className="text-red-600">{errorMessage}</p>}
+      {successMessage && (
+        <p className="text-green-600">Sign Up is Successful.</p>
+      )}
     </div>
   );
 };
