7b55ed2e613ed8881314d9365c8e454fc8afb1dd
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-11-10 16:53:56 +0600

    components added, register form doing

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className="max-w-5xl mx-auto">
             <Header></Header>
             <Outlet></Outlet>
         </div>
